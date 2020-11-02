71a85055149b8bdd7ce9c63014424cc2736dbd09
Izabela-Zelek <C00247865@itcarlow.ie>
2020-11-02 01:26:49 +0000

    Rabbid attacks partly done

--- MarioVsRabbit/main.cpp
+++ MarioVsRabbit/main.cpp
@@ -2,35 +2,21 @@
 #include "main.h"
 
 using namespace std;
- //Plunger - rabbid whacks mario with a toiler plunger
+///Melee:
+/// 
+/// Plunger - rabbid whacks mario with a toiler plunger
+/// Shark Attack - Rabbid will cosplay as a shark and bite player
+/// Now-you-see-me - Rabbid sticks plunger on player's face, leave round shaped bruises
+/// 
 
 /// <summary>
-///these attacks will all be using carrots as the point is to overfeed the rabbits til they're too full to fight
-/// Melee: 
-/// Stomp - mario jumps on the rabbit's head
-/// Uppercut - mario punches rabbit's chin
-/// Wrench - mario whacks the rabbit in the head, a bump forms
-/// Groundpound - mario double jumps and stomps on the ground, rabbit is knocked off feet
-/// Cape slash - mario throws his cape like a whip
-/// Saw Surprise - mario cuts hole around rabbit, rabbit falls down and jumps back up
-/// Mr Nose-it-all - mario pretends to take rabbit's nose, hurts itself in its confusion
+/// Abilities
+/// Plunger Gun - uses a gun to shoot plungers
+/// Boutta Steal - Rabbid can steal one of player's turns
+/// Siren's Song - Rabbit will 'sing' badly and give Player a headache
+/// Energy Drink - Rabbit mutates and squashes player by sitting on them
+/// Tactical Ruse - Rabbit cosplays as princess plum, player facepalms so hard they hurt themself
 /// 
-/// Ability:
-/// Carrot Archery - mario uses a bow to shoot carrots at the rabbit
-/// Carrot Cake - mario makes a carrot cake so delicious, the rabbit can't help but eat it all
-/// Carrot Trap - mario leads rabbit into trap
-/// Carrot Temptation - mario baits the rabbit into following beside carrot patch, rabbit can't resist and eats some
-/// Carrot Yeet - mario throws carrots at the rabbit...that's it
-/// 
-/// Defense:
-/// Cape parry - cape acts as a shield and gives half the damage back to the rabbit (can only be used against melee)
-/// Starman - mario receives no damage if the attack is an ability
-/// Shroom Soup - mario can replenish some health
-/// </summary>
-
-Plumber player;
-Rabbid rabbids[10];
-
 int main(void)
 {
 	rabbids[m_currentEnemy].generateHealth();
@@ -55,54 +41,53 @@ int main(void)
 	//the game loop, enemies will spawn as long as the player doesn't have all achievements
 	while (m_numAchievements < M_MAX_ACHIEVEMENTS)
 	{
-		int enemyHealth = rabbids[m_currentEnemy].healthShow();
-		cout << enemyHealth<< "\n";
-		player.healthShow();
-		player.xpShow();
-		player.playerChoice();
-		rabbids[m_currentEnemy].receiveDamage();
-	
-		//rabbids turn
-
+		player.turn();
+		rabbids[m_currentEnemy].turn();
 		//player.move();
 		//player.receiveDamage();
-
-
-
-
-
-
 	}
-
 	cin.get();
 }
 
 void Plumber::receiveDamage()
 {
 }
-
+/// <summary>
+/// Displays the player's health on screen
+/// </summary>
+/// <returns></returns>
 int Plumber::healthShow()
 {
 	cout << showName() << "'s health - " << m_health << "	";
 
 	return 0;
 }
-
+/// <summary>
+/// Displays the player's XP on screen
+/// </summary>
 void Plumber::xpShow()
 {
 	cout << showName() << "'s XP -  " << m_xp << "\n";
 }
-
+/// <summary>
+/// Used to confirm player's chosen character
+/// </summary>
+/// <param name="t_name"></param>
 void Plumber::addName(string t_name)
 {
 	m_name = t_name;
 }
-
+/// <summary>
+/// returns player's name
+/// </summary>
+/// <returns></returns>
 string Plumber::showName()
 {
 	return m_name;
 }
-
+/// <summary>
+/// Asks player what option they would want to choose in battle
+/// </summary>
 void Plumber::playerChoice()
 {
 	int action;
@@ -128,7 +113,9 @@ void Plumber::playerChoice()
 		playerChoice();
 	}
 }
-
+/// <summary>
+/// Lists all player melee attacks and displays text accordingly
+/// </summary>
 void Plumber::melee()
 {
 	system("Cls");
@@ -140,7 +127,7 @@ void Plumber::melee()
 	{
 		cout << m_name << m_meleeAttack[choice] << "\n";
 		m_damageGiven = rand() % M_MAX_MELEE;
-		cout << m_damageGiven;
+		cout << "Rabbit loses " << m_damageGiven << " health\n";
 		system("Pause");
 		system("Cls");
 	}
@@ -149,10 +136,8 @@ void Plumber::melee()
 		melee();
 	}
 }
-/// Defense:
-/// Cape parry - cape acts as a shield and gives half the damage back to the rabbit (can only be used against melee)
-/// Starman - mario receives no damage if the attack is an ability
-/// Shroom Soup - mario can replenish some health
+/// <summary>
+/// Lists all player defense attacks and displays text accordingly
 /// </summary>
 void Plumber::defense()
 {
@@ -165,7 +150,7 @@ void Plumber::defense()
 	{
 		cout << m_name << m_defenseAttack[choice] << "\n";
 		m_damageGiven = rand() % 10;
-		cout << m_damageGiven;
+		cout << "Rabbit loses " << m_damageGiven << " health\n";
 		system("Pause");
 		system("Cls");
 	}
@@ -173,7 +158,10 @@ void Plumber::defense()
 	{
 		defense();
 	}
-} 
+}
+/// <summary>
+/// Lists all player ability attacks and displays text accordingly
+/// </summary>
 void Plumber::abilities()
 {
 	system("Cls");
@@ -185,7 +173,7 @@ void Plumber::abilities()
 	{
 		cout << m_name << m_abilityAttack[choice] << "\n";
 		m_damageGiven = rand() % M_MAX_ABILITY + M_MIN_ABILITY;
-		cout << m_damageGiven << "\n";
+		cout << "Rabbit loses " << m_damageGiven << " health\n";
 		system("Pause");
 		system("Cls");
 	}
@@ -195,7 +183,19 @@ void Plumber::abilities()
 	}
 
 }
+/// <summary>
+/// determines what happens during player's turn
+/// </summary>
+void Plumber::turn()
+{
+	player.healthShow();
+	player.xpShow();
+	player.playerChoice();
 
+}
+/// <summary>
+/// calculates the remaining health of the rabbits and makes new enemy
+/// </summary>
 void Rabbid::receiveDamage()
 {
 	m_health -= m_damageGiven;
@@ -205,10 +205,126 @@ void Rabbid::receiveDamage()
 		m_enemyAlive[m_currentEnemy] = false;
 		m_currentEnemy++;
 		rabbids[m_currentEnemy].generateHealth();
+		if (m_enemyAlive[M_MAX_ENEMY - 1] == false)
+		{
+			for (int i = 0; i < M_MAX_ENEMY; i++)
+			{
+				m_enemyAlive[i] == true;
+				rabbids[i].generateHealth();
+			}
+			m_currentEnemy = 0;
+		}
 	}
 }
-
+/// <summary>
+/// Randomly generates rabbit health
+/// </summary>
 void Rabbid::generateHealth()
 {
 	m_health = rand() % 20 + 50;
 }
+
+/// <summary>
+/// randomly generates rabbit option choice
+/// </summary>
+void Rabbid::rabbidChoice()
+{
+	int randChoice = rand() % 3;
+	if (randChoice == 0)
+	{
+		melee();
+	}
+	else if (randChoice == 1)
+	{
+		defense();
+	}
+	else if (randChoice == 2)
+	{
+		abilities();
+	}
+	else
+	{
+		system("Cls");
+		rabbidChoice();
+	}
+}
+/// <summary>
+/// generates what melee attack the rabbid uses
+/// </summary>
+void Rabbid::melee()
+{
+}
+/// <summary>
+/// generates what defense the rabbid uses
+/// </summary>
+void Rabbid::defense()
+{
+}
+/// Abilities
+/// Plunger Gun - uses a gun to shoot plungers
+/// Boutta Steal - Rabbid can steal one of player's turns
+/// Siren's Song - Rabbit will 'sing' badly and give Player a headache
+/// Energy Drink - Rabbit mutates and squashes player by sitting on them
+/// Tactical Ruse - Rabbit cosplays as princess plum, player facepalms so hard they hurt themself
+/// 
+/// <summary>
+/// generates what ability attack the rabbid uses
+/// </summary>
+void Rabbid::abilities()
+{
+	system("Cls");
+	int abChoice = rand() % 5;
+
+	cout << m_abilityRabbid[abChoice] << "\n";
+	
+		//m_damageGiven = rand() % M_MAX_ABILITY + M_MIN_ABILITY;
+		//cout << "Rabbit lost " << m_damageGiven << " health\n";
+	system("Pause");
+	if (abChoice == 1)
+	{
+		turn();
+	}
+	system("Cls");
+	
+}
+
+/// <summary>
+/// determines what happens during rabbid turn
+/// </summary>
+void Rabbid::turn()
+{
+	rabbids[m_currentEnemy].receiveDamage();
+	int enemyHealth = rabbids[m_currentEnemy].healthShow();
+	cout << "Rabbit's health is now at " << enemyHealth << "\n";
+	rabbidChoice();
+	system("Pause");
+	system("Cls");
+}
+
+
+
+
+
+/// <summary>
+///these attacks will all be using carrots as the point is to overfeed the rabbits til they're too full to fight
+/// Melee: 
+/// Stomp - mario jumps on the rabbit's head
+/// Uppercut - mario punches rabbit's chin
+/// Wrench - mario whacks the rabbit in the head, a bump forms
+/// Groundpound - mario double jumps and stomps on the ground, rabbit is knocked off feet
+/// Cape slash - mario throws his cape like a whip
+/// Saw Surprise - mario cuts hole around rabbit, rabbit falls down and jumps back up
+/// Mr Nose-it-all - mario pretends to take rabbit's nose, hurts itself in its confusion
+/// 
+/// Ability:
+/// Carrot Archery - mario uses a bow to shoot carrots at the rabbit
+/// Carrot Cake - mario makes a carrot cake so delicious, the rabbit can't help but eat it all
+/// Carrot Trap - mario leads rabbit into trap
+/// Carrot Temptation - mario baits the rabbit into following beside carrot patch, rabbit can't resist and eats some
+/// Carrot Yeet - mario throws carrots at the rabbit...that's it
+/// 
+/// Defense:
+/// Cape parry - cape acts as a shield and gives half the damage back to the rabbit (can only be used against melee)
+/// Starman - mario receives no damage if the attack is an ability
+/// Shroom Soup - mario can replenish some health
+/// </summary>
--- MarioVsRabbit/main.h
+++ MarioVsRabbit/main.h
@@ -5,9 +5,6 @@
 using namespace std;
 
 const int M_MAX_ACHIEVEMENTS = 30;
-const int M_MAX_MELEE = 11;
-const int M_MAX_ABILITY = 20;
-const int M_MIN_ABILITY = 10;
 const int M_MAX_ENEMY = 10;
 int m_chosenChar;
 int m_numAchievements = 0;
@@ -15,22 +12,32 @@ int m_damageGiven;
 int m_currentEnemy = 0;
 bool m_enemyAlive[M_MAX_ENEMY]{ true,true,true,true,true,true,true,true,true,true };
 
+
 string m_meleeAttack[7]{ " jumps up and bonks Rabbit with their hell."," whacks Rabbit in the chin, Rabbid sees a pretty shape in the clouds.",
 					   " whacks Rabbit in the head with their wrench, an ugly bump forms."," lands backside first on the ground, a mini earthquake knocks Rabbid off their feet.",
 					   " uses their cape to shoo Rabbit away, Rabbit gets mildly hurt."," jumps on Rabbit's back and cuts hole around Rabbit, Rabbit falls down and bounces back up.",
 					   " pretends to take Rabbit's nose, Rabbit hurts itself in its confusion."};
 
 string m_abilityAttack[5]{ " throws carrots at the Rabbit's feet in desperation."," uses a bow and shoots carrots into the Rabbit's mouth. Rabbit rubs their stomach.",
-						   " baits Rabbit into walking beside a carrot patch, Rabbit can't resist the temptation. "," leads Rabbit into a carrot filled trap",
+						   " baits Rabbit into walking beside a carrot patch, Rabbit can't resist the temptation. "," leads Rabbit into a carrot filled trap.",
 						   " bakes a carrot cake so delicious, Rabbit can't resist and eats it all."};
 
 string m_defenseAttack[3]{ "'s cape wraps around him, all melee attacks will be blocked once. Rabbit's next attack will be parried with half the damage.",
 						   " manifested a Super Star, all ability attacks will be blocked once. Rabbit's next ability attack will be parried with part of the damage",
-						   " uses Shroom Soup. Health has been restored" };
+						   " uses Shroom Soup. Health has been restored." };
+
+string m_abilityRabbid[5]{ "Rabbit uses gun! Plungers shoot out and hit you in the stomach.","Rabbit has stolen your turn! You also got a smack on the wrist. Ouch.",
+						   "Rabbit's are unfortunately not known for their singing abilities. You witnessed this firsthand. Your ears are bleeding now.",
+						   "Rabbit somehow found an energy drink! He grows big and blue. Rabbit has successfully used you as a chair!",
+						   "Rabbits love cosplay. A rabbit shaped Princess Plum has appeared where the rabbit once stood. You facepalm." };
 class Character {
 public:
 	void move() { cout << "I-a move my legs" << endl; }
 	virtual int healthShow() { cout << "Health unknown" << endl; return 0; }
+	virtual void melee() { cout << "I can swing my sword"; }
+	virtual void defense() { cout << "Block out the haters"; }
+	virtual void abilities() { cout << "I use my brain"; }
+	virtual void turn() { cout << "It's MY turn on the XBOX"; }
 	void xpShow() { cout << "XP unknown" << endl; }
 };
 
@@ -45,11 +52,15 @@ public:
 	void melee();
 	void defense();
 	void abilities();
+	void turn();
 
 private:
 	string m_name = "";
 	int m_health = 50;
 	int m_xp = 0;
+	const int M_MAX_MELEE = 11;
+	const int M_MAX_ABILITY = 20;
+	const int M_MIN_ABILITY = 10;
 };
 
 class Rabbid : public Character {
@@ -57,9 +68,16 @@ public:
 	void receiveDamage();
 	void generateHealth();
 	int healthShow() { return m_health; }
+	void rabbidChoice();
+	void melee();
+	void defense();
+	void abilities();
+	void turn();
 
 private:
 	int m_health;
 };
 
+Plumber player;
+Rabbid rabbids[10];
 	
